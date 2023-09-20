b4c2c96e7c0ba703bbf0cffa77b505b8bd8af42c
Aleksandr_UB <bogdanov777000@mail.ru>
2023-09-20 19:38:00 +0300

    fix

--- src/app/app.selector.ts
+++ src/app/app.selector.ts
@@ -1,4 +1,5 @@
 import {AppRootStateType} from "app/store";
 
 export  const selectAppStatus  = (state: AppRootStateType) => state.app.status
+
 export  const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized
